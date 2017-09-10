368343611562e450b6c0d4f2d5a677e2a83b39e5
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-09-10 21:04:08 +0200

    .

--- ci/prepare.sh
+++ ci/prepare.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+apt-get -qq update
+apt-get install software-properties-common
 add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 apt-get -qq update
 apt-get -qq install --no-install-recommends g++-7
