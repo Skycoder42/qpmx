adbd8012649f05764eccd78b2aedc168e63bf570
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-09-11 12:32:42 +0200

    try standard gcc (5)

--- ci/travis_init.sh
+++ ci/travis_init.sh
@@ -5,7 +5,7 @@ currDir=$(dirname $0)
 
 # move data into docker
 if [[ $PLATFORM == "gcc_64" ]]; then
-	echo 'QMAKE_CXX=g++-6' >> .qmake.conf
+	#echo 'QMAKE_CXX=g++-6' >> .qmake.conf
 	mv ./qtmodules-travis/ci/linux/build-all.sh ./qtmodules-travis/ci/linux/build-all-orig.sh
 	mv $currDir/prepare.sh ./qtmodules-travis/ci/linux/build-all.sh
 fi
