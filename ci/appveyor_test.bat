48f34f735d97ab7ca73e305eca86d6e1fa9b59d6
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-09-12 13:14:30 +0200

    wtf

--- ci/appveyor_test.bat
+++ ci/appveyor_test.bat
@@ -3,14 +3,17 @@ setlocal
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64 || exit /B 1
 
+@echo on
 set qtplatform=%PLATFORM%
 set PATH=C:\Qt\%QT_VER%\%qtplatform%\bin;%PATH%;%CD%\build-%qtplatform%\qpmx;
 
 :: install plugins into qt
-@echo on
+echo current
 dir
+echo cd build-%qtplatform%
 cd build-%qtplatform%
 dir
+echo cd plugins
 cd plugins
 dir
 
