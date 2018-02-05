14ee025bf53783d6132ca3079718f4907bdf58a0
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-02-05 19:17:45 +0100

    .

--- ci/appveyor_deploy.bat
+++ ci/appveyor_deploy.bat
@@ -1,8 +1,11 @@
 :: deploy
 setlocal
+@echo on
 
 :: "normal" deployment
+dir
 cd install
+dir
 move bin/qpmx.exe qpmx.exe || exit \b 1
 rmdir bin
 C:\projects\Qt\%QT_VER%\%qtplatform%\bin\windeployqt.exe --release --no-translations qpmx.exe || exit \b 1
@@ -10,3 +13,5 @@ C:\projects\Qt\%QT_VER%\%qtplatform%\bin\windeployqt.exe --release --no-translat
 :: create qt.conf
 echo [Paths] > qt.conf
 echo Prefix=. >> qt.conf
+
+dir
